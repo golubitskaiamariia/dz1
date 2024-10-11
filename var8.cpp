f5ce641c756e09a5fe3db822ed87df45f81c9b10
mary <mary.g.200617@gmail.com>
2024-10-11 20:52:18 +0300

    First

---
+++ var8.cpp
@@ -0,0 +1,25 @@
+// Угол альфа задан в радианах. Найти его величину в градусах, минутах и секундах. 
+
+#include <iostream>
+#include <cmath> 
+
+int main()
+{
+    double radian, degree; // Объявляем переменные с плавающей точкой. 
+
+    std::cout << "Enter the angle in radians: \n"; // Вводим исходные данные угла
+    std::cin >> radian;
+
+    degree = radian * (180.0 / M_PI); // Рассчитываем в градусах
+    int d = static_cast<int>(degree); // Получаем целую часть градусов
+    
+    double minutesOfDegree = (degree - d) * 60.0; // Получаем дробную часть и преобразуем в минуты
+    int m = static_cast<int>(minutesOfDegree); // Получаем целую часть минут
+
+    double secondsOfMinutes = (minutesOfDegree - m) * 60.0; // Получаем дроб5ную часть и преобразуем в секунды
+    int s = static_cast<int>(secondsOfMinutes); // Получаем целую часть секунд
+    
+    std::cout << radian <<" radian = " << d << " ° " << m << " ' " << s << " '' " << std::endl; //Вывод значения
+
+    return 0;
+}
---
+++ var8_2.cpp
@@ -0,0 +1,66 @@
+//Заданы стороны треугольника: a, b, c. Проверить, является ли этот треугольник остроугольным.
+
+#include <iostream>
+#include <cmath> 
+using namespace std;
+int main()
+{
+    double a, b, c; // Объявляем переменные. 
+    cout << "Еnter side a: \n"; // Вводим исходные данные стороны а
+    cin >> a;
+    cout << "Еnter side b: \n"; // Вводим исходные данные стороны b
+    cin >> b;
+    cout << "Еnter side c: \n"; // Вводим исходные данные стороны c
+    cin >> c;
+
+    if (a == b){
+        if(a > c){
+        cout << "A triangle is an acute triangle1";
+        }
+        if(c > a){
+        cout << "A triangle is not an acute triangle2" << endl;
+        }
+    }
+    if (a == c){
+        if(a > b){
+        cout << "A triangle is an acute triangle3";
+        }
+        if(a < b){
+        cout << "A triangle is not an acute triangle4";
+        }
+    }
+    if (b == c){
+        if(b > a){
+        cout << "A triangle is an acute triangle5";
+        }
+        if(b < a){
+        cout << "A triangle is not an acute triangle6";
+        }
+    }
+    
+    if((a == b) && (a==c) && (b==c)){
+        cout << "A triangle is an acute triangle7";
+    }
+
+    if((a > b) && (a > c) && (b != c)){ //Проверяем сторону а на наибольшее значение
+        if(pow(a, 2) < (pow(b, 2) + pow(c, 2))){ // Проверяем, чтобы квадрат наибольшей стороны был меньше суммы квадратов двух остальных
+            cout << "A triangle is an acute triangle8";
+        }
+        else{ cout << "A triangle is not an acute triangle9";}
+    }
+    if((b > a) && (b > c) && (a != c)){ //Проверяем сторону а на наибольшее значение
+        if(pow(b, 2) < (pow(a, 2) + pow(c, 2))){ // Проверяем, чтобы квадрат наибольшей стороны был меньше суммы квадратов двух остальных
+            cout << "A triangle is an acute triangle10";
+        }
+        else{ cout << "A triangle is not an acute triangle11";}
+    }
+    if((c > a) && (c > b) && (a != b)){ //Проверяем сторону а на наибольшее значение
+        if(pow(c, 2) < (pow(a, 2) + pow(b, 2))){ // Проверяем, чтобы квадрат наибольшей стороны был меньше суммы квадратов двух остальных
+            cout << "A triangle is an acute triangle12";
+        }
+        else{ cout << "A triangle is not an acute triangle13";}
+
+    }
+
+    return 0;
+}
